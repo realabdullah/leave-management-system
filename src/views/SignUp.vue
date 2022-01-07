80eef4d2af01682768066a0a06a61a3d5c1fd6a1
realabdullah <abdulodesanmi@gmail.com>
2022-01-07 20:44:13 +0100

    bug

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -43,7 +43,7 @@
           </div>
           <div>
             <label for="password" class="sr-only">Password</label>
-            <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
+            <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password (Password should be six characters or more.)" />
           </div>
         </div>
 
@@ -59,7 +59,7 @@
         </div>
 
         <div>
-          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <button v-if="password.length > 5" type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
               <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
             </span>
@@ -151,6 +151,8 @@ export default {
     const loading = ref(false)
     const errMsg = ref('')
     const requested = ref(false)
+    
+    const passLength = password.value.length
 
     // const store = reactive({
     //   user: {}
@@ -218,6 +220,7 @@ export default {
       password,
       loading,
       errMsg,
+      passLength,
       // signUp
       requestSignup,
       requested
