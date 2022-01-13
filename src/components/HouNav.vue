1a17e88822dc7ded9b4d2e2a3b75581fca5aa871
realabdullah <abdulodesanmi@gmail.com>
2022-01-13 15:40:01 +0100

    hou comp

--- src/components/HouNav.vue
+++ src/components/HouNav.vue
@@ -14,17 +14,13 @@
         </div>
         <div class="hidden md:block">
           <div class="ml-4 flex items-center md:ml-6">
-            <button type="button" class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-              <span class="sr-only">View notifications</span>
-              <BellIcon class="h-6 w-6" aria-hidden="true" />
-            </button>
 
             <!-- Profile dropdown -->
             <Menu as="div" class="ml-3 relative">
               <div>
                 <MenuButton class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                   <span class="sr-only">Open user menu</span>
-                  <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
+                  <span>Menu</span>
                 </MenuButton>
               </div>
               <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
---
+++ src/views/Hou.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="min-h-full">
+    <HouNav />
+    <StaffOverview />
+  </div>
+</template>
+
+<script>
+import HouNav from '../components/HouNav.vue'
+import StaffOverview from '../components/StaffOverview.vue'
+
+export default {
+  components: {
+    HouNav,
+    StaffOverview
+  }
+}
+</script>
+
+<style>
+
+</style>
