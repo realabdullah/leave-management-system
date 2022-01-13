7c89903669dfb8a447a9573161f468431a055e48
realabdullah <abdulodesanmi@gmail.com>
2022-01-13 20:03:45 +0100

    bug

--- src/components/AdminNav.vue
+++ src/components/AdminNav.vue
@@ -47,15 +47,9 @@
 
     <DisclosurePanel class="md:hidden">
       <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-        <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+        <DisclosureButton v-for="item in navigation" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
       </div>
       <div class="pt-4 pb-3 border-t border-gray-700">
-        <div class="flex items-center px-5">
-          <div class="ml-3">
-            <div class="text-base font-medium leading-none text-white">{{ user.name }}</div>
-            <div class="text-sm font-medium leading-none text-gray-400">{{ user.email }}</div>
-          </div>
-        </div>
         <div class="mt-3 px-2 space-y-1">
           <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">
             <span v-if="item.name != 'Sign out'">
