ec55ff72b250ff32cde2503f0a0089873f4dae71
realabdullah <abdulodesanmi@gmail.com>
2022-01-13 16:33:29 +0100

    .

--- src/components/HouProfile.vue
+++ src/components/HouProfile.vue
@@ -5,7 +5,7 @@
     <div v-for="profile in houData" id="profile" class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
       <div class="p-4 md:p-12 text-center lg:text-left">
         <!-- Image for mobile view-->
-        <div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('https://source.unsplash.com/MP0IUfwrn0A')"></div>
+        <!-- <div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('https://source.unsplash.com/MP0IUfwrn0A')"></div> -->
         
         <h1 class="text-3xl font-bold pt-8 lg:pt-0">{{ profile.name }}</h1>
         <div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-indigo-500 opacity-25"></div>
@@ -20,15 +20,12 @@
             </router-link>
           </button> 
         </div>
-        <!-- Use https://simpleicons.org/ to find the svg for your preferred product --> 
       </div>
     </div>
     <!--Img Col-->
-    <div class="w-full lg:w-2/5">
-      <!-- Big profile image for side bar (desktop) -->
+    <!-- <div class="w-full lg:w-2/5">
       <img src="https://source.unsplash.com/MP0IUfwrn0A" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block">
-      <!-- Image from: http://unsplash.com/photos/MP0IUfwrn0A -->
-    </div>
+    </div> -->
   </div>
 </template>
 
