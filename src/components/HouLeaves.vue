9d22c2e5db07e199e31aa19c1b80ef590e123012
realabdullah <abdulodesanmi@gmail.com>
2022-01-13 16:25:32 +0100

    hou

--- src/components/HouLeaves.vue
+++ src/components/HouLeaves.vue
@@ -24,9 +24,6 @@
                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Leave Status
                 </th>
-                <th scope="col" class="relative px-6 py-3">
-                  <span class="sr-only">Edit</span>
-                </th>
               </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
@@ -102,7 +99,7 @@ export default {
         if (error) {
           console.log(error)
         } else {
-          // await all()
+          await all()
           // console.log(houDetails.value)
           // console.log(department.value)
         }
