3bc8ccb61a3bbab7e047a31fad58a1f00da2c6bf
realabdullah <abdulodesanmi@gmail.com>
2022-03-09 22:36:28 +0100

    Admin Leave action

--- src/views/LeaveAction.vue
+++ src/views/LeaveAction.vue
@@ -90,7 +90,7 @@
           </div>
           <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt class="text-sm font-medium text-gray-500">
-              HOU Response
+              HOU Action
             </dt>
             <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
               {{ details.hou_status }}
@@ -98,18 +98,18 @@
           </div>
           <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt class="text-sm font-medium text-gray-500">
-              Staus
+              HOU Comment
             </dt>
             <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              {{ details.status }}
+              {{ details.hou_comment }}
             </dd>
           </div>
           <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt class="text-sm font-medium text-gray-500">
-              HOU Response
+              Current Status
             </dt>
             <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              {{ details.hou_status }}
+              {{ details.status }}
             </dd>
           </div>
           <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
